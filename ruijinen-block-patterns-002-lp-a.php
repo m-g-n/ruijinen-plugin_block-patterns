f6c7a6a804a6f9e31f041bde337f734f36c5e237
アクツユミ <akutsu@m-g-n.me>
2021-05-27 15:55:21 +0900

    RUIJINEN-382 [LP：パターンプラグイン] 伝えたいこと（ノーマル）パターン構築

--- ruijinen-block-patterns-002-lp-a.php
+++ ruijinen-block-patterns-002-lp-a.php
@@ -62,6 +62,8 @@ add_filter( 'rje_register_patterns_args', 'rje_P002lp_hero_media_and_text', 10 )
 add_filter( 'rje_register_patterns_args', 'rje_P002lp_hero_media_and_text__alignright', 10 );
 add_filter( 'rje_register_patterns_args', 'rje_P002lp_hero_one_column', 10 );
 add_filter( 'rje_register_patterns_args', 'rje_P002lp_message_normal', 10 );
+add_filter( 'rje_register_patterns_args', 'rje_P002lp_message_normal__alignright', 10 );
+add_filter( 'rje_register_patterns_args', 'rje_P002lp_message_accent', 10 );
 
 function rje_P002lp_hero_media_and_text ( $args ) {
 	$args[] = array(
@@ -96,6 +98,24 @@ function rje_P002lp_message_normal ( $args ) {
 		'title' => '伝えたいこと（ノーマル)',
 		'cat'   => array( RJE_P002LP_KEY ),
 		'style' => array( RJE_P002LP_KEY.'_message_normal' ),
+	);
+	return $args;
+}
+function rje_P002lp_message_normal__alignright ( $args ) {
+	$args[] = array(
+		'key'   => RJE_P002LP_KEY . '_message_normal__alignright',
+		'title' => '伝えたいこと（ノーマル) - 右寄せ',
+		'cat'   => array( RJE_P002LP_KEY ),
+		'style' => array( RJE_P002LP_KEY.'_message_normal' ),
+	);
+	return $args;
+}
+function rje_P002lp_message_accent ( $args ) {
+	$args[] = array(
+		'key'   => RJE_P002LP_KEY . '_message_accent',
+		'title' => '伝えたいこと（アクセント)',
+		'cat'   => array( RJE_P002LP_KEY ),
+		'style' => array( RJE_P002LP_KEY.'_message_accent' ),
 	);
 	return $args;
 }
