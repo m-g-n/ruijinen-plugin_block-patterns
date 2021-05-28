7481007a923805ff8dce035a4df5967252175f7e
アクツユミ <akutsu@m-g-n.me>
2021-05-28 11:46:26 +0900

    RUIJINEN-380 [LP：パターンプラグイン] 伝えたいこと（アクセント）パターン構築
    スタイル設定

--- patterns/RJE_P002LP_message_accent/pattern.php
+++ patterns/RJE_P002LP_message_accent/pattern.php
@@ -1,5 +1,5 @@
-<!-- wp:snow-monkey-blocks/section -->
-<div class="wp-block-snow-monkey-blocks-section smb-section"><div class="smb-section__inner"><div class="c-container"><div class="smb-section__subtitle">MATCH FOR YOU</div><h2 class="smb-section__title">MATCHをおすすめしたい方</h2><div class="smb-section__body"><!-- wp:snow-monkey-blocks/media-text {"titleTagName":"h3","mediaId":39,"mediaLink":"http://pattern-dev-ruijinen.local/hero%e3%82%a4%e3%83%a1%e3%83%bc%e3%82%b8%ef%bc%882%e3%82%ab%e3%83%a9%e3%83%a0%ef%bc%89/bg/","mediaType":"image","mediaColumnSize":"50"} -->
+<!-- wp:snow-monkey-blocks/section {"className":"is-style-RJE_P002LP_message_accent"} -->
+<div class="wp-block-snow-monkey-blocks-section smb-section is-style-RJE_P002LP_message_accent"><div class="smb-section__inner"><div class="c-container"><div class="smb-section__subtitle">MATCH FOR YOU</div><h2 class="smb-section__title">MATCHをおすすめしたい方</h2><div class="smb-section__body"><!-- wp:snow-monkey-blocks/media-text {"titleTagName":"h3","mediaId":39,"mediaLink":"http://pattern-dev-ruijinen.local/hero%e3%82%a4%e3%83%a1%e3%83%bc%e3%82%b8%ef%bc%882%e3%82%ab%e3%83%a9%e3%83%a0%ef%bc%89/bg/","mediaType":"image","mediaColumnSize":"50"} -->
 <div class="wp-block-snow-monkey-blocks-media-text smb-media-text"><div class="c-row c-row--margin c-row--middle"><div class="c-row__col c-row__col--1-1 c-row__col--lg-1-2"><h3 class="smb-media-text__title">髪の悩みに向き合うあなたへ</h3><div class="smb-media-text__body"><!-- wp:paragraph -->
 <p>MATCHは、ひとりひとりの髪の悩みや好みを聴いて、シャンプーをおすすめします。</p>
 <!-- /wp:paragraph -->
