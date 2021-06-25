ff0d73288988293c8d0c8ceb81d73b2c9d5485f2
アクツユミ <akutsu@m-g-n.me>
2021-06-26 00:21:08 +0900

    ヘルパーブロックパターン（スタイル）の提供 #50
    - フル幅時に余白がつくパターンを追加
    - フル幅時にコンテナ幅がコンテンツ幅になるパターンを追加

---
+++ inc/register-helper-patterns.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * ヘルパーブロックパターンの登録処理
+ * 
+ * @package ruijinen-block-patterns
+ * @author mgn
+ * @license GPL-2.0+
+ */
+
+namespace Ruijinen\Pattern\HelperPattern;
+
+class RegisterHelperPatterns {
+
+	public function __construct() {
+		define( 'RJE_R000HELPER_KEY', 'RJE_R000HELPER' ); // どの類人猿プロダクトなのかを示すキー
+		$this->init();
+	}
+
+	/**
+	 * ブロックパターンの登録に関する処理を実行する
+	 */
+	public function init() {
+		add_action( 'init', array( $this, 'register_pattern_cat' ), 10 ); //パターンカテゴリー登録
+		add_action( 'plugins_loaded', array( $this, 'register_add_pettern_args' ) ); //ヘルパーパターンの情報を追加
+
+		//ヘルパーパターン共通のスタイルを読み込む
+		// add_action(
+		// 	'wp_enqueue_scripts',
+		// 	function () {
+		// 		$path = 'dist/css/sample-pattern-common.css';
+		// 		wp_enqueue_style( RJE_R000HELPER_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path, array( 'snow-monkey', 'snow-monkey-blocks', 'snow-monkey-snow-monkey-blocks', 'snow-monkey-blocks-background-parallax' ), filemtime( RJE_BP_PLUGIN_PATH . $path ) );
+		// 	},
+		// 	10
+		// );
+		// add_action(
+		// 	'enqueue_block_editor_assets',
+		// 	function () {
+		// 		$path = 'dist/css/sample-pattern-common.css';
+		// 		wp_enqueue_style( RJE_R000HELPER_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path,  array( 'snow-monkey-snow-monkey-blocks-editor' ), filemtime( RJE_BP_PLUGIN_PATH . $path ) );
+		// 	},
+		// 	10
+		// );
+	}
+
+	/**
+	* ヘルパーパターンのカテゴリを登録
+	*/
+	public function register_pattern_cat () {
+		register_block_pattern_category( RJE_R000HELPER_KEY, array( 'label' => '[類人猿] ヘルパー' ) );
+	}
+
+	/**
+	* ヘルパーパターンの情報を追加
+	*/
+	public function register_add_pettern_args() {
+		add_filter( 'rje_register_patterns_args', array( $this, 'rje_r000helper_fullwidth_min_padding' ), 10 );
+		add_filter( 'rje_register_patterns_args', array( $this, 'rje_r000helper_fullwidth_max_width' ), 10 );
+	}
+	public function rje_r000helper_fullwidth_min_padding ( $args ) {
+		$args[] = array(
+			'key'            => RJE_R000HELPER_KEY . '_fullwidth_min_padding',
+			'title'          => 'フル幅時に最低限余白を保持',
+			'cat'            => array( RJE_R000HELPER_KEY ),
+			'specific-style' => false,
+			'block-style'    => array( RJE_R000HELPER_KEY . '_fullwidth_min_padding' ),
+			'path'           => RJE_BP_PLUGIN_PATH,
+		);
+		return $args;
+	}
+	public function rje_r000helper_fullwidth_max_width ( $args ) {
+		$args[] = array(
+			'key'            => RJE_R000HELPER_KEY . '_fullwidth_max_width',
+			'title'          => 'フル幅時にコンテンツ幅になる',
+			'cat'            => array( RJE_R000HELPER_KEY ),
+			'specific-style' => false,
+			'block-style'    => array( RJE_R000HELPER_KEY . '_fullwidth_max_width' ),
+			'path'           => RJE_BP_PLUGIN_PATH,
+		);
+		return $args;
+	}
+}
+
+new RegisterHelperPatterns();
