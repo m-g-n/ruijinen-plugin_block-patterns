c8fdc9a69a99a88dad32282735a62b607dd6712f
アクツユミ <akutsu@m-g-n.me>
2022-04-20 18:02:45 +0900

    バージョン番号の桁を繰り上げ

--- ruijinen-block-patterns.php
+++ ruijinen-block-patterns.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: 類人猿ブロックパターン
  * Description: Snow Monkeyサイトをより素敵にするブロックパターンを提供
- * Version: 0.0.1.9
+ * Version: 1.10.0
  * Tested up to: 5.9
  * Requires at least: 5.9
  * Author: mgn Inc.,
