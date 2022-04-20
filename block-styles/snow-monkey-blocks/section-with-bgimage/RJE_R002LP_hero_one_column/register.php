52df4baa1185da50ee3a89dd2faf0fbb8b91bbb6
アクツユミ <akutsu@m-g-n.me>
2022-04-20 19:40:29 +0900

    gulpからの脱却 #116
    - gulpでのコンパイルからDart Sassへ切り替え
    - 編集画面用のCSSの読み込み方法の変更

--- block-styles/snow-monkey-blocks/section-with-bgimage/RJE_R002LP_hero_one_column/register.php
+++ block-styles/snow-monkey-blocks/section-with-bgimage/RJE_R002LP_hero_one_column/register.php
@@ -7,9 +7,12 @@
 
 $override_block_name = 'snow-monkey-blocks/section-with-bgimage';
 $block_style_label   = '類人猿R002_LP Hero（1カラム)';
+
+//各ファイルパスなどを設定.
 $basename = basename( __DIR__ );
-$front_filename  = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/style-front.css';
-$editor_filename = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/style-editor.css';
+$dist_dir_path = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/';
+$front_filename  = $dist_dir_path . 'style-front.css';
+$editor_filename = $dist_dir_path . 'style-editor.css';
 
 $front_filetime = ( file_exists( RJE_BP_PLUGIN_PATH . $front_filename ) ) ? filemtime( RJE_BP_PLUGIN_PATH . $front_filename ) : NULL;
 $editor_filetime = ( file_exists( RJE_BP_PLUGIN_PATH . $editor_filename ) ) ? filemtime( RJE_BP_PLUGIN_PATH . $editor_filename ) : NULL;
@@ -23,6 +26,8 @@ register_block_style(
 	)
 );
 
-//フロント・エディター用のCSSファイルを登録
+//フロント用のCSSファイルを登録
 wp_register_style( 'is-style-' . $basename . '-front', RJE_BP_PLUGIN_URL . $front_filename, $this->style_front_deps, $front_filetime );
-wp_register_style( 'is-style-' . $basename . '-editor', RJE_BP_PLUGIN_URL . $editor_filename, $this->style_editor_deps, $editor_filetime );
+
+//エディター用のCSSファイルを登録
+add_editor_style('../../plugins/'.RJE_BP_DIRNAME.'/'.$front_filename);
