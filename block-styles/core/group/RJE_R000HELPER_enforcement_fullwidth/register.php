ff4a02f5b95bade00d95e9690fbc6ac23ba93f3e
アクツユミ <akutsu@m-g-n.me>
2022-05-18 16:21:48 +0900

    サンプルパターンの依存ハンドルの取得方法の変更（プロパティ名を最新のものへ差し替え） #142

--- block-styles/core/group/RJE_R000HELPER_enforcement_fullwidth/register.php
+++ block-styles/core/group/RJE_R000HELPER_enforcement_fullwidth/register.php
@@ -27,8 +27,8 @@ register_block_style(
 );
 
 //フロント用のCSSファイルを登録
-wp_register_style( 'is-style-' . $basename . '-front', RJE_BP_PLUGIN_URL . $front_filename, $this->style_front_deps, $front_filetime );
+wp_register_style( 'is-style-' . $basename . '-front', RJE_BP_PLUGIN_URL . $front_filename, $this->sm_style_handles, $front_filetime );
 
 //エディター用のCSSファイルを登録
 add_editor_style('../../plugins/'.RJE_BP_DIRNAME.'/'.$front_filename);
-wp_register_style( 'is-style-' . $basename . '-editor', RJE_BP_PLUGIN_URL . $editor_filename, $this->style_editor_deps, $editor_filetime );
+wp_register_style( 'is-style-' . $basename . '-editor', RJE_BP_PLUGIN_URL . $editor_filename, $this->sm_style_handles, $editor_filetime );
