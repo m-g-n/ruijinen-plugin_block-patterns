5a35bf1057396bd736d79a13246d27a6e3e3649f
アクツユミ <akutsu@m-g-n.me>
2022-05-18 16:49:01 +0900

    サンプルパターンのCSSがeditorで読み込まれているのをトル #141
    handleの取得方法の変更

--- App/Patterns/RegisterSamplePattern.php
+++ App/Patterns/RegisterSamplePattern.php
@@ -12,17 +12,9 @@ namespace Ruijinen\Pattern\Master\App\Patterns;
 class RegisterSamplePatterns {
 
 	/**
-	 * プロパティ
-	 */
-	public $sm_style_handles  = ''; //Snow Monkey のメインスタイルのハンドルを格納.
-
-	/**
 	 * construct
 	 */
 	public function __construct() {
-		if ( method_exists('\Framework\Helper', 'get_main_style_handle') ) {
-			$this->sm_style_handles = \Framework\Helper::get_main_style_handle();
-		}
 		define( 'RJE_R000SAMPLE_KEY', 'RJE_R000SAMPLE' ); // どの類人猿プロダクトなのかを示すキー
 		$this->init();
 	}
@@ -38,16 +30,22 @@ class RegisterSamplePatterns {
 		add_action(
 			'wp_enqueue_scripts',
 			function () {
-				$path             = 'dist/css/sample-pattern-common.css';
-				wp_enqueue_style( RJE_R000SAMPLE_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path, $this->sm_style_handles, filemtime( RJE_BP_PLUGIN_PATH . $path ) );
+				if ( method_exists('\Framework\Helper', 'get_main_style_handle') ) {
+					$sm_style_handles = \Framework\Helper::get_main_style_handle();
+				}
+				$path = 'dist/css/sample-pattern-common.css';
+				wp_enqueue_style( RJE_R000SAMPLE_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path, $sm_style_handles, filemtime( RJE_BP_PLUGIN_PATH . $path ) );
 			},
 			10
 		);
 		add_action(
 			'enqueue_block_editor_assets',
 			function () {
+				if ( method_exists('\Framework\Helper', 'get_main_style_handle') ) {
+					$sm_style_handles = \Framework\Helper::get_main_style_handle();
+				}
 				$path = 'dist/css/sample-pattern-common.css';
-				wp_enqueue_style( RJE_R000SAMPLE_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path, $this->sm_style_handles, filemtime( RJE_BP_PLUGIN_PATH . $path ) );
+				wp_enqueue_style( RJE_R000SAMPLE_KEY . 'sample-pattern-common', RJE_BP_PLUGIN_URL . $path, $sm_style_handles, filemtime( RJE_BP_PLUGIN_PATH . $path ) );
 			},
 			10
 		);
