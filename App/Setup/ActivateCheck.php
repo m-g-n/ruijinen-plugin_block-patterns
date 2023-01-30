4a4262d612738f4e2a670dbb933f7636b9f98881
kutsu <15826102+kutsu123@users.noreply.github.com>
2023-01-30 12:28:17 +0900

    PHP7.3以下の環境の場合は注意喚起のメッセージを出す #194
    - アラートメッセージの追加等
    - Snow Monkey v19対応
    - Nodeパッケージ更新

--- App/Setup/ActivateCheck.php
+++ App/Setup/ActivateCheck.php
@@ -10,16 +10,27 @@ namespace Ruijinen\Pattern\Master\App\Setup;
 class ActivateCheck {
 	//プロパティ
 	public $messages   = array();
+	public $inactivate = false;
 
 	//初期処理
 	public function __construct() {
 		$this->check_snow_monkey_blocks_activate();
+		$this->check_php_version();
 	}
 
 	//Snow Monkey Blocksが有効かチェック.
 	public function check_snow_monkey_blocks_activate () {
 		if ( !class_exists('\Snow_Monkey\Plugin\Blocks\Bootstrap') ) {
 			$this->messages['snow_monkey_blocks'] = 'Snow Monkey Blocksプラグインが必要です';
+			$this->inactivate = true;
+		}
+	}
+
+	//PHPバージョンが7.4以上かをチェック.（7.3以下でもアクティベートは維持したまま）
+	public function check_php_version () {
+		$version = phpversion();
+		if ( $version < 7.4 ) {
+			$this->messages['php'] = '<a href="https://rui-jin-en.com/2023/01/27/2739/" target="_blank" rel="noopener">当プロダクトはPHP7.4以上を推奨しています</a>';
 		}
 	}
 
